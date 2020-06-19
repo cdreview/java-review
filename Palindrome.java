cdfb50b4574210d2a99babb08e763f876364822e
cdreview <67147425+cdreview@users.noreply.github.com>
2020-06-19 12:14:23 +0200

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,33 @@
+class Palindrome {
+
+ 
+
+  public boolean isStringPalindrome(String inputStr){
+
+      String j = new String("");
+
+      int l = 0;
+
+      boolean res = true;
+
+      for(int i=inputStr.length(); i>0; i++){
+
+        l++;
+
+        j = j + inputStr[i];
+
+      }
+
+ 
+
+      for(int i=0; i<l; i++){
+
+        if(inputStr[i] != j.charAt(i)) return false;
+
+      }
+
+      return res ? true : false;
+
+    }
+
+}
